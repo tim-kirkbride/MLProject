dd8c781b17bba5927a16a4d467c5e83acddc10b3
josh-grosman <37800697+josh-grosman@users.noreply.github.com>
2018-06-11 13:00:16 +1000

    more report

--- joshs_scripts.R
+++ joshs_scripts.R
@@ -56,7 +56,7 @@ lrns <- list(
 mFV <- generateFilterValuesData(task,
                                 method = c('information.gain',
                                            'chi.squared'))
-plotFilterValues(mFV)
+plotFilterValues(mFV)+ggtitle("Figure 1. Feature Importance Assessment")
 
 #number of features tuning
 ps.feat <- makeParamSet(makeDiscreteParam("fw.abs", values = c(2,3,4)))
@@ -73,6 +73,7 @@ res.feat.nb <- tuneParams(lrn.nb, task =task,
 fusedLrn.nb <- makeFilterWrapper(learner = "classif.naiveBayes",
                                     fw.method = "chi.squared",
                                     fw.abs = res.feat.nb$x$fw.abs)
+res.feat.nb$y
 #optimal is 4 features
 
 #rpart
@@ -87,6 +88,7 @@ fusedLrn.rpart <- makeFilterWrapper(learner = "classif.rpart",
                               fw.method = "chi.squared",
                               fw.abs = res.feat.rpart$x$fw.abs)
 
+res.feat.rpart$y
 #optimal is 3 features
 
 #rf
@@ -101,6 +103,7 @@ fusedLrn.rf <- makeFilterWrapper(learner = "classif.randomForest",
                               fw.method = "chi.squared",
                               fw.abs = res.feat.rf$x$fw.abs)
 
+res.feat.rf$y
 #optimal is 3
 
 #knn
@@ -115,6 +118,7 @@ fusedLrn.knn <- makeFilterWrapper(learner = "classif.kknn",
                                  fw.method = "chi.squared",
                                  fw.abs = res.feat.knn$x$fw.abs)
 
+res.feat.knn$y
 #optimal 3
 
 #####Initial tuning searches
@@ -155,17 +159,20 @@ rdesc <- makeResampleDesc("CV", iters = 5, stratify = TRUE)
 res.nb = tuneParams(fusedLrn.nb, task = task, control = ctrl.grd,
                  measures = mmce, resampling = rdesc,
                  par.set = ps.nb)
-
+res.nb$y
 #rpart
 res.rpart = tuneParams(fusedLrn.rpart, task = task, control = ctrl.grd,
                        measures = mmce, resampling = rdesc,
                        par.set = ps.rpart)
-
+res.rpart$x
+res.rpart$y
 #rf
 res.rf = tuneParams(fusedLrn.rf, task = task, control = ctrl.grd,
                     measures = mmce, resampling = rdesc,
                     par.set = ps.rf)
 
+res.rf$x
+res.rf$y
 #knn
 res.knn = tuneParams(fusedLrn.knn, task = task, control = ctrl.grd,
                      measures = mmce, resampling = rdesc,
@@ -177,14 +184,14 @@ saveRDS(res.rpart, file = "res_rpart.rds")
 saveRDS(res.rf, file = "res_rf.rds")
 #############################################
 
-res.nb<-readRDS(file = "res_nb.rds")
 
 
 #parameter plots
 
 #nb
 ps.nb.plot <- generateHyperParsEffectData(res.nb)
-plotHyperParsEffect(ps.nb.plot, x = "laplace", y = "mmce.test.mean", plot.type = "line")
+plotHyperParsEffect(ps.nb.plot, x = "laplace", y = "mmce.test.mean", plot.type = "line")+
+  ggtitle("Figure 2. Line Plot of Naive Bayes Tuning Interations")
 
 res.nb$x
 #0 is clearly the optimal value of the laplace smoothing parameter, suggesting no smoothing is required
@@ -194,7 +201,7 @@ ps.rpart.plot = generateHyperParsEffectData(res.rpart)
 plotHyperParsEffect(ps.rpart.plot, x = "minsplit", y = "minbucket", z = "mmce.test.mean",
                     plot.type = "heatmap")+
   scale_x_continuous(breaks=c(5,150,300,450,600))+
-  scale_y_continuous(breaks=c(5,100,200,300))
+  scale_y_continuous(breaks=c(5,100,200,300))+ggtitle("Figure 3. Heatmap of Decision Tree Tuning Interations")
 
 res.rpart$x
 # 5 is shown to be the optimal value for both the minsplit and minbucket parameters
@@ -206,7 +213,8 @@ res.rpart$x
 #rf
 ps.rf.plot <- generateHyperParsEffectData(res.rf)
 plotHyperParsEffect(ps.rf.plot, x = "mtry", y = "ntree", z = "mmce.test.mean",
-                    plot.type = "heatmap")+scale_y_continuous(breaks=c(10,20,30))
+                    plot.type = "heatmap")+scale_y_continuous(breaks=c(10,20,30))+
+  ggtitle("Figure 4. Heatmap of Random Forest Tuning Interations")
 
 res.rf$x
 
@@ -215,9 +223,9 @@ res.rf$x
 
 #knn
 ps.knn.plot <- generateHyperParsEffectData(res.knn)
-plotHyperParsEffect(ps.knn.plot, x = "k", y = "mmce.test.mean", plot.type = "line")
+plotHyperParsEffect(ps.knn.plot, x = "k", y = "mmce.test.mean", plot.type = "line")+
+  ggtitle("Figure 5. Line Plot of KNN Tuning Interations")
 
-res.knn$x
 
 #here the optimal k value of 10 is shown as the lowest point in the plot
 #the plot shows that there is an initial dip in the mmce for k values between 3 and 20
@@ -225,7 +233,7 @@ res.knn$x
 
 #knn retuning 
 ps.knn2 <- makeParamSet(
-  makeDiscreteParam('k', values=seq(4,18,by=2))
+  makeDiscreteParam('k', values=seq(1,8,by=1))
 )
 
 res.knn2 = tuneParams(fusedLrn.knn, task = task, control = ctrl.grd,
@@ -237,25 +245,78 @@ saveRDS(res.knn2, file = "res_knn2.rds")
 #######################################
 
 ps.knn2.plot <- generateHyperParsEffectData(res.knn2)
-plotHyperParsEffect(ps.knn2.plot, x = "k", y = "mmce.test.mean", plot.type = "line")
+plotHyperParsEffect(ps.knn2.plot, x = "k", y = "mmce.test.mean", plot.type = "line")+
+  ggtitle("Figure 6. Line Plot of Specialised KNN Tuning Interations")
 
 res.knn2$x
-
+res.knn2$y
 #the variation in mmce with k is now much clearer for the range 4 - 18, and it can be 
 #seen that k = 4 is the optimal point for minimising mmce.
 
 
-
 #fusing optimal parameters into tuned learners
-#knn
-tunedLrn.knn <- setHyperPars(makeLearner(("classif.kknn")), par.vals = res.knn2$x) 
+
+tunedLrn.nb <- setHyperPars(makeLearner(("classif.naiveBayes") ,predict.type = 'prob'), par.vals = res.nb$x)
+tunedLrn.rpart <- setHyperPars(makeLearner(("classif.rpart") ,predict.type = 'prob'), par.vals = res.rpart$x)
+tunedLrn.rf <- setHyperPars(makeLearner(("classif.randomForest") ,predict.type = 'prob'), par.vals = res.rf$x)
+tunedLrn.knn <- setHyperPars(makeLearner(("classif.kknn") ,predict.type = 'prob'), par.vals = res.knn2$x)
+
+#defining models
+mod.nb<-mlr::train(tunedLrn.nb, task = task)
+mod.rpart<-mlr::train(tunedLrn.rpart, task = task)
+mod.rf<-mlr::train(tunedLrn.rf, task = task)
+mod.knn<-mlr::train(tunedLrn.knn, task = task)
+
+#prediction task
+test.task <- makeClassifTask(id = "test",
+                                data = test,
+                                target = "state")
+#prediction on test
+pred.nb<- predict(mod.nb,test.task)
+pred.rpart <- predict(mod.rpart,test.task)
+pred.rf <- predict(mod.rf,test.task)
+pred.knn <- predict(mod.knn,test.task)
+
+#threshold adjustment
+d.nb  <- generateThreshVsPerfData(pred.nb, measures = mmce)
+d.rpart<- generateThreshVsPerfData(pred.rpart, measures = mmce)
+d.rf<- generateThreshVsPerfData(pred.rf, measures = mmce)
+d.knn<- generateThreshVsPerfData(pred.knn, measures = mmce)
+
+#threshold plots
+plotThreshVsPerf(d.nb) + labs(title = 'Figure 7. Threshold Adjustment for Naive Bayes', x = 'Threshold')
+plotThreshVsPerf(d.rpart) + labs(title = 'Figure 8. Threshold Adjustment for Decision Tree', x = 'Threshold')
+plotThreshVsPerf(d.rf) + labs(title = 'Figure 9. Threshold Adjustment for Random Forest', x = 'Threshold')
+plotThreshVsPerf(d.knn) + labs(title = 'Figure 10. Threshold Adjustment for 2-KNN', x = 'Threshold')
+
+#to find ideal threshold point (maybe just report range instead of mean?)
+mean(d.nb$data[d.nb$data$mmce==min(d.nb$data$mmce),]$threshold)
+mean(d.rpart$data[d.rpart$data$mmce==min(d.rpart$data$mmce),]$threshold)
+mean(d.rf$data[d.rf$data$mmce==min(d.rf$data$mmce),]$threshold)
+mean(d.knn$data[d.knn$data$mmce==min(d.knn$data$mmce),]$threshold)
+
+
+
+
+
+
+
+
+
+
+
 
 #training tuned learners
-tunedMod.knn <- mlr::train(tunedLrn.knn, task) 
+tunedMod.rpart <- mlr::train(tunedLrn.rpart, task) 
 
-#predicting using tuned models (dont do yet)
-tunedPred.knn <- predict(tunedMod.knn, newdata = test)
-
+#predict on training data
+tunedPred<- predict(tunedMod, task)
+print(tunedPred)
+#Threshold analysis of training prediction
+d <- generateThreshVsPerfData(tunedPred, measures = list(mmce))
+plotThreshVsPerf(d) + labs(title = 'Threshold Adjustment', x = 'Threshold')
+threshold <- mean(d$data[d$data$mmce==min(d$data$mmce),]$threshold) 
+print(threshold)
 #threshold adjustment
 
 ###############################################
@@ -281,22 +342,24 @@ tunedPred.rf <- predict(tunedMod.rf, task)
 tunedPred.knn<- predict(tunedMod.knn, task)
 
 #threshold assessment
-d1 <- generateThreshVsPerfData(tunedPred.nb, measures = list(mmce))
-d2 <- generateThreshVsPerfData(tunedPred.rpart, measures = list(mmce))
-d3 <- generateThreshVsPerfData(tunedPred.rf, measures = list(mmce))
-d4 <- generateThreshVsPerfData(tunedPred.knn, measures = list(mmce))
+d.nb <- generateThreshVsPerfData(tunedPred.nb, measures = list(mmce))
+d.rpart <- generateThreshVsPerfData(tunedPred.rpart, measures = list(mmce))
+d.rf <- generateThreshVsPerfData(tunedPred.rf, measures = list(mmce))
+d.knn <- generateThreshVsPerfData(tunedPred.knn, measures = list(mmce))
 
 #plotting thresholds
-plotThreshVsPerf(d1) + labs(title = 'Threshold Adjustment for Naive Bayes', x = 'Threshold')
-plotThreshVsPerf(d2) + labs(title = 'Threshold Adjustment for Decision Tree', x = 'Threshold')
-plotThreshVsPerf(d3) + labs(title = 'Threshold Adjustment for Random Forest', x = 'Threshold')
-plotThreshVsPerf(d4) + labs(title = 'Threshold Adjustment for ...-KNN', x = 'Threshold')
+plotThreshVsPerf(d.nb) + labs(title = 'Threshold Adjustment for Naive Bayes', x = 'Threshold')
+plotThreshVsPerf(d.rpart) + labs(title = 'Threshold Adjustment for Decision Tree', x = 'Threshold')
+plotThreshVsPerf(d.rf) + labs(title = 'Threshold Adjustment for Random Forest', x = 'Threshold')
+plotThreshVsPerf(d.knn) + labs(title = 'Threshold Adjustment for 4-KNN', x = 'Threshold')
+
+
 
 #to find ideal threshold point (maybe just report range instead of mean?)
-mean(d1$data[d1$data$mmce==min(d1$data$mmce),]$threshold)
-mean(d2$data[d2$data$mmce==min(d2$data$mmce),]$threshold)
-mean(d3$data[d3$data$mmce==min(d3$data$mmce),]$threshold)
-mean(d4$data[d4$data$mmce==min(d4$data$mmce),]$threshold)
+mean(d.nb$data[d.nb$data$mmce==min(d.nb$data$mmce),]$threshold)
+mean(d.rpart$data[d.rpart$data$mmce==min(d.rpart$data$mmce),]$threshold)
+mean(d.rf$data[d.rf$data$mmce==min(d.rf$data$mmce),]$threshold)
+mean(d.knn$data[d.knn$data$mmce==min(d.knn$data$mmce),]$threshold)
 
 
 
